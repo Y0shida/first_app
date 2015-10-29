01355645cc1e8d6f8461bdf770ab540c86be99a8
吉田有希 <e145753@yoshidayuki-no-macbook-pro.local>
2015-10-29 12:12:12 +0900

    Initial Commit

---
+++ Group_A/Group_A.h
@@ -0,0 +1,19 @@
+//
+//  Group_A.h
+//  Group_A
+//
+//  Created by 吉田有希 on 2015/10/29.
+//  Copyright © 2015年 Yuki Yoshida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Group_A.
+FOUNDATION_EXPORT double Group_AVersionNumber;
+
+//! Project version string for Group_A.
+FOUNDATION_EXPORT const unsigned char Group_AVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Group_A/PublicHeader.h>
+
+
